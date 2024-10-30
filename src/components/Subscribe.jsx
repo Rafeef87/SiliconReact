1dd14a4ed6e5143353cb8642da0fd17e7a686e23
Rafeef87 <rafeefkhalifah1987@gmail.com>
2024-10-30 17:20:05 +0100

    Send data to webApi for the newsletter.

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -1,23 +1,40 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 
 import notification from '../assets/images/notification-icon-.svg'
+import { useForm } from 'react-hook-form'
+
 
 const Subscribe = () => {
-/*en enklare form av validering*/
-const [email, setEmail] = useState('')
-const [error, setError] = useState('')
+/*Form av validering*/
 
-const handleSubscribe = (e) => {
-  e.preventDefault();
-  if(!email) {
-    setError('Please enter a valid email address.')
-  } else {
-    setError('')
+const [submitted, setSubmitted] = useState(false)
+const {register, handleSubmit, reset, formState: { errors }} = useForm()
 
-    console.log('Subscribed with email: ', email)
-    setEmail('') //Clear the entry after subscription
-  }
+
+const handleOk = () => {
+  setSubmitted(false)
+  reset()
 }
+const onSubmit = async (data) => {
+    const res = await axios.post('https://win24-assignment.azurewebsites.net/api/forms/subscribe', data)
+
+  if(res.status === 200) {
+    setSubmitted(true)
+    reset()
+  }
+  
+}
+
+  if (submitted) {
+    return (
+    <div className="container informatbox">
+      <h1>Thanks for your subscribe</h1>
+      <button className='btn-primary' onClick={handleOk}>OK</button>
+    </div>
+    )
+  }
+
 
   return (
     <section id="subscribe">
@@ -27,12 +44,16 @@ const handleSubscribe = (e) => {
                         <h2 className="hide-text">Subscribe to our newsletter </h2>
                         <h2 className="show">Subscribe to our newsletter to stay informed about latest updates</h2>
                     </div>
-                    <div className="email">
-                        <i className="fa-light fa-envelope email-icon"></i>
-                        <input className="mail" type="email" value={email} onChange={(e) => setEmail(e.target.value)} required placeholder=" Your email" />
-                        <a onClick={handleSubscribe} id="subscribe" href="#" className="btn-subscribe">Subscribe</a>
-                    </div>
-                    {error && <p className='error-message'>{error}</p>}
+                    <form onSubmit={handleSubmit(onSubmit)} noValidate className="email">
+                      <div className='form-group'>
+                          <i className="fa-light fa-envelope email-icon"></i>
+                          <input className="mail" type="email" {...register('email', { required: 'The email field is required'})}  placeholder=" Your email" />
+                          <span>{errors.email && errors.email.message}</span>
+                      </div>
+                      <button type='submit' id="subscribe" className="btn-subscribe">Subscribe</button>
+                        
+                    </form>
+                    
                 </div>
     </section>
   )
